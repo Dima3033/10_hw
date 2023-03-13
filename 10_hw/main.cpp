738cb517f1beac23c3cee67a07ec3ce8c1ff85fd
Dima3033 <114282314+Dima3033@users.noreply.github.com>
2023-03-13 20:09:25 +0200

    commit

--- 10_hw/main.cpp
+++ 10_hw/main.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 int* arri() 
 {
-	static int a[5]; 
-	for (int i = 0; i < 5; i++)
+	static int a[10]; 
+	for (int i = 0; i < 10; i++)
 	{
 		a[i] = i; 
 	}
-	return a;
+	return a; 
 }
 int main()
 {
-	int* funk; 
+	int* funk;
 	int i;
 	funk = arri();
-	cout << "Array is -> ";
-	for (i = 0; i < 5; i++)
-		cout << funk[i] << "\t"; 
+	cout << "Array is: ";
+	for (i = 0; i < 10; i++)
+		cout << funk[i] << "\t";
 
 	return 0;
 }
