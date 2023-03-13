4c08cb689c7d1f7738e702db00a6ef9cbf44951c
Dima3033 <114282314+Dima3033@users.noreply.github.com>
2023-03-13 20:24:36 +0200

    commit

--- 10_hw/main.cpp
+++ 10_hw/main.cpp
@@ -1,22 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int* arri() 
-{
-	static int a[5]; 
-	for (int i = 0; i < 5; i++)
-	{
-		a[i] = i; 
-	}
-	return a;
+int* insert_block(int* arr, int size, int index, int* block, int block_size) {
+    int new_size = size + block_size;
+    int* new_arr = new int[new_size];
+    for (int i = 0; i < index; i++) {
+        new_arr[i] = arr[i];
+    }
+    for (int i = 0; i < block_size; i++) {
+        new_arr[index + i] = block[i];
+    }
+    for (int i = index; i < size; i++) {
+        new_arr[block_size + i] = arr[i];
+    }
+    for (int i = 0; i < size; i++) {
+        new_arr[block_size + i] = arr[i];
+    }
+    delete[] arr;
+    return new_arr;
 }
-int main()
-{
-	int* funk; 
-	int i;
-	funk = arri();
-	cout << "Array is -> ";
-	for (i = 0; i < 5; i++)
-		cout << funk[i] << "\t"; 
-
-	return 0;
+int main() {
+    int size = 6;
+    int* arr = new int[size] {34,75,46,11,2};
+    int block_size = 3;
+    int* block = new int[block_size] {11,55,87};
+    int index = 0;
+    arr = insert_block(arr, size, index, block, block_size);
+    size += block_size;
+    cout << "Result ->" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    delete[] arr;
+    return 0;
 }
